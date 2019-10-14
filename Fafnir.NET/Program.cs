15c7f2df3608a88b65f78eba34e695005199f0c9
MechaDragonX <sonicraghav@gmail.com>
2019-10-14 10:21:17 -0700

    XOR encrypt strings

--- Fafnir.NET/Program.cs
+++ Fafnir.NET/Program.cs
@@ -6,7 +6,25 @@ namespace Fafnir.NET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+        private static int GenKey()
+        {
+            Random rng = new Random();
+            return rng.Next(256);
+        }
+        private static string XORString(string input)
+        {
+            int key;
+            string output = input;
+            int current;
+            for(int i = 0; i < output.Length; i++)
+            {
+                key = GenKey();
+                current = output[i];
+                current ^= key;
+                output.Replace(output[i], (char)current);
+            }
+            return output;
         }
     }
 }
